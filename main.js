bf92086c1598deb201a56b8e1308d9e67b2d9e30
pavan444hanuman <58776544+pavan444hanuman@users.noreply.github.com>
2019-12-11 19:07:36 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ let myImage = document.querySelector('img');
 myImage.onclick = function()
 {
 	let imageSource = myImage.getAttribute('src');
-	if(imageSource === 'images/pavan.jpg')
-		myImage.setAttribute('src','images/hanuman.JPEG');
+	if(imageSource === 'pavan.jpg')
+		myImage.setAttribute('src','hanuman.JPEG');
 	else
-		myImage.setAttribute('src','images/pavan.jpg');
+		myImage.setAttribute('src','pavan.jpg');
 }
 
 let myHeader = document.querySelector('h1');
